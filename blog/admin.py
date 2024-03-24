ab162d08e8cab62666a9c5f6726331b96f1f7868
mb8504 <mbergmarketing@gmail.com>
2024-03-24 19:32:38 -0400

    customized admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Title/Date', {'fields': ['title', 'date_posted']}),
+        ('Content', {'fields': ['content']}),
+        ('Author', {'fields': ['author']}),
+    ]
+
+
+
+admin.site.register(Post, PostAdmin)
