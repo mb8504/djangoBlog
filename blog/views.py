ff138e66abe5d7f85934290c37a48a6bc2ce9638
mb8504 <mbergmarketing@gmail.com>
2024-03-25 19:30:02 -0400

    finished templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
+
+posts = [
+    {
+        'author': 'MikeyB',
+        'title': 'Blog post 1',
+        'content': 'March 25, 2024',
+    },
+    {
+        'author': 'TraveyB',
+        'title': 'My Blog Post',
+        'content': 'March 25, 2024',
+    }
+]
+
+
+
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+
+
